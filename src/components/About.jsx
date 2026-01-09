80097d391c4979ce3fbee4386e3125d3898845d9
phoebe lau <phoebelau2603@gmail.com>
2026-01-09 11:09:32 +0800

    Update 1.3.5
    corrected bus schedule,
    added description and bus schedule pic into About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 function About() {
     return(
-        <div>
-            <h1>about</h1>
-            <h2>coming soon...</h2>
-        </div>
+        <>
+            <div className="content">
+                <h2>About</h2>
+                <p>This app was created to help residents of Tapestry Condo in Tampines, Singapore keep track of the shuttle bus schedule. It provides real-time updates on bus arrival times and allows users to view the schedule for different days of the week. Do note that this is an individual project so there may be mistakes here and there.</p>
+                <br />
+                <p>Note: This schedule is based on the one released on 1st Dec 2025</p>
+                <br />
+                <p className="content-notes">Tap the image to see the full version</p>
+                <a href={`${import.meta.env.BASE_URL}schedule/bus_dec2025.jpeg`} target="_blank" rel="noopener noreferrer">
+                    <img src={`${import.meta.env.BASE_URL}schedule/bus_dec2025.jpeg`} alt="Dec'25 Bus Schedule"  style={{width: '200px'}}/>
+                </a>
+            </div>
+        </>
     );
 }
 
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function Weather() {
     return(
-        <div>
-            <h1>weather</h1>
-            <h2>coming soon...</h2>
-        </div>
+        <>
+            <div className="content">
+                <h1>weather</h1>
+                <h2>coming soon...</h2>
+            </div>
+        </>
     );
 }
 
