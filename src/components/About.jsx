5a7b477b0c335280b4e4b6f5c7768f01d24d5f92
memelord26 <phoebelau2603@gmail.com>
2026-07-28 21:56:26 +0800

    update bus schedule July'26

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,11 +7,11 @@ function About() {
                 <h1>About</h1>
                 <p>This app was created to help residents of Tapestry Condo in Tampines, Singapore keep track of the shuttle bus schedule. It provides real-time updates on bus arrival times and allows users to view the schedule for different days of the week. Do note that this is an individual project so there may be mistakes here and there.</p>
                 <br />
-                <p>Note: This schedule is based on the one released on 1st Dec 2025</p>
+                <p>Note: This schedule is based on the one released on 15th July 2026</p>
                 <br />
                 <p className="content-notes">Tap the image to see the full version</p>
-                <a href={`${import.meta.env.BASE_URL}schedule/bus_dec2025.jpeg`} target="_blank" rel="noopener noreferrer">
-                    <img src={`${import.meta.env.BASE_URL}schedule/bus_dec2025.jpeg`} alt="Dec'25 Bus Schedule"  style={{width: '200px'}}/>
+                <a href={`${import.meta.env.BASE_URL}schedule/bus_july2026.png`} target="_blank" rel="noopener noreferrer">
+                    <img src={`${import.meta.env.BASE_URL}schedule/bus_july2026.png`} alt="Dec'25 Bus Schedule"  style={{width: '200px'}}/>
                 </a>
             </div>
         </>
